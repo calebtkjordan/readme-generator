8de893dd83112b8aacaf2203b820a48dd53aa619
calebtkjordan <123994285+calebtkjordan@users.noreply.github.com>
2024-02-19 20:03:06 +0000

    added questions

--- index.js
+++ index.js
@@ -2,20 +2,100 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const util = require('util');
 
-// array of questions for user
-const questions = [
+// READ DOCUMENATION - ALL ANSWERS ARE IN DOCUMENTATION https://www.npmjs.com/package/inquirer
+// CHECK SOLUTION TO MINI PROJECT - VERY SIMILAR TO THIS PROJECT
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'what is the project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the purpose of this project?',
+        },
+        {
+            type: 'input',
+            name: 'contents',
+            message: 'Would you like to include a table of contents?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How should this project be installed?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What license is this project distributed under?',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'How can others contribute to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What tests are available for this project?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Provice contact information for questions about the project.',
+        },
+    ]);
 
-// function to initialize program
-function init() {
+const generateReadMe = (answers) =>
+`
+# ${answers.title}
 
-}
+## Description
 
-// function call to initialize program
-init();
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## License
+
+${answers.license}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+${answers.questions}
+`;
+
+promptUser()
+    .then((answers) => writeFileAsync('README.md', generateReadMe(answers)))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
